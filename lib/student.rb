925488960d70aa1bef7a682fe67bc786b45b23b9
Dan Powell <powell956@gmail.com>
2021-05-13 17:47:43 -0400

    commit tree 142cb7f0f1515c0705e1cb5922b4a76e7e81d0dd

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User 
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(tidbit) 
+        @knowledge << tidbit
+    end
+
+    def knowledge 
+        @knowledge
+    end
 
 end
